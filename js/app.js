1fa653df0451b8c2c3ed5bb54686075c4bcc8229
Mborjas <Borjascastillo@hotmail.com>
2018-11-17 22:30:38 -0500

    segundo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
+
+// if ( navigator.serviceWorker ) {
+//     var swLocation = '/sw.js';
+//     navigator.serviceWorker.register( swLocation );
+// }
+
+
 if ( navigator.serviceWorker ) {
-    var swLocation = '/sw.js';
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
     navigator.serviceWorker.register( swLocation );
 }
 
